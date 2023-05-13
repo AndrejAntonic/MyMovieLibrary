7dc50e43536e50656fa90410169b625940da6576
aantonic20 <100687011+aantonic20@users.noreply.github.com>
2023-05-13 18:09:58 +0200

    Add project files.

---
+++ MyMovieLibrary/Models/Movies.cs
@@ -0,0 +1,8 @@
+﻿namespace MyMovieLibrary.Models
+{
+    public struct DataMovies
+    {
+        public string Original_title { get; set; }
+        public string Image { get; set; }
+    }
+}
---
+++ MyMovieLibrary/ViewModels/LibraryViewModel.cs
@@ -0,0 +1,47 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using MyMovieLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using Wpf.Ui.Common.Interfaces;
+
+namespace MyMovieLibrary.ViewModels
+{
+    public partial class LibraryViewModel : ObservableObject, INavigationAware
+    {
+        private bool _isInitialized = false;
+
+        [ObservableProperty]
+        private IEnumerable<DataMovies> _movies;
+
+        public void OnNavigatedFrom()
+        {
+        }
+
+        public void OnNavigatedTo()
+        {
+            if (!_isInitialized)
+                InitializeViewModel();
+
+        }
+
+        private void InitializeViewModel()
+        {
+            var moviesCollection = new List<DataMovies>();
+
+            moviesCollection.Add(new DataMovies
+            {
+                Original_title = "Avatar",
+                Image = "../../Assets/jRXYjXNq0Cs2TcJjLkki24MLp7u.jpg"
+            });
+
+            Movies = moviesCollection;
+
+            _isInitialized = true;
+        }
+    }
+}
